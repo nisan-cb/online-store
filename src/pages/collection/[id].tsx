d82f2fa1f3f8772b743c2c99978148181643b728
Nisan <nnisan858@gmail.com>
2023-04-28 18:55:16 +0300

    add utils folders

--- src/pages/collection/[id].tsx
+++ src/pages/collection/[id].tsx
@@ -3,8 +3,8 @@ import Header from "@/components/header/header";
 import Head from "@/components/head/head";
 import Main from "@/components/main/main";
 import Gallery from "@/components/main/components/gallery/gallery";
-import { getStaticPaths } from "./utils/getStaticPaths";
-import { getStaticProps } from "./utils/getStaticProps";
+// import { getStaticPaths } from "./utils/getStaticPaths";
+// import { getStaticProps } from "./utils/getStaticProps";
 
 
 const CategoryPage: FC<CollectionPageProps> = ({ categoryName }) => {
@@ -20,6 +20,33 @@ const CategoryPage: FC<CollectionPageProps> = ({ categoryName }) => {
     )
 }
 
-export { getStaticPaths }
-export { getStaticProps }
+// export { getStaticPaths }
+// export { getStaticProps }
 export default CategoryPage;
+
+import { GetStaticPaths } from 'next'
+import config from "../../config.json";
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    const paths = config.categories;
+
+    const mappedPaths = paths.map((path) => ({ params: { id: path } }));
+
+    // Return the paths object
+    return {
+        paths: mappedPaths,
+        fallback: false,
+    };
+}
+
+import { GetStaticProps } from "next";
+
+
+export const getStaticProps: GetStaticProps<CollectionPageProps> = async ({ params }) => {
+    const categoryName = params?.id as string;
+
+    return { props: { categoryName } };
+};
+
+
+
--- src/pages/collection/utils/getStaticProps.ts
+++
@@ -1,9 +0,0 @@
-import { GetStaticProps } from "next";
-
-
-export const getStaticProps: GetStaticProps<CollectionPageProps> = async ({ params }) => {
-    const categoryName = params?.id as string;
-
-    return { props: { categoryName } };
-};
-

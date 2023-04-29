0eab31a2bd4023f108a8becaa10bff91350d1f8d
Nisan <nnisan858@gmail.com>
2023-04-29 10:50:32 +0300

    serverSideProps

--- src/pages/item/[id].tsx
+++ src/pages/item/[id].tsx
@@ -1,15 +1,13 @@
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
-import db from "@/pages/api/db/db";
+import db from "@/db/db";
 import Main from '@/components/main/main'
 import { FC } from 'react'
 import Head from '@/components/head/head'
 import Header from '@/components/header/header'
 import SingleItem from '@/components/main/components/singleItem/singleItem'
+import { getServerSideProps, PageProps } from "./getServerSideProps"
 
 
-interface PageProps {
-    item: SingleItemData
-}
 
 const Item: FC<PageProps> = ({ item }) => {
     const { id, tittle } = item;
@@ -24,26 +22,6 @@ const Item: FC<PageProps> = ({ item }) => {
     )
 }
 
-
-
-const getServerSideProps: GetServerSideProps<PageProps> = async (context: GetServerSidePropsContext) => {
-    const params = context.params as { id: string };
-    const { id } = params;
-    console.log("id: ", id)
-    const data = await db.getItemById(id);
-    console.log(data);
-    // const data = {
-    //     id: "4dasdad45",
-    //     tittle: "tittle",
-    //     price: 111,
-    //     mainImage: ""
-    // };
-
-    if (!data)
-        return { notFound: true };
-
-    return { props: { item: data } };
-}
 
 export { getServerSideProps }
 export default Item;
